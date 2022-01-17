245ce2faecc03ebebf1323c99fc16e28399f8b9a
divyans-GitHub <divyanshuji123456@gmail.com>
2022-01-17 15:19:17 +0530

    project setup Ready!

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+function App() {
+  return (
+    <div className="App">
+      <strong> Codeial App 'S Folder Structure Is Ready  </strong>
+    </div>
+  );
+}
+
+export default App;
